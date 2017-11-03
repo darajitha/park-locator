4c0425a0892721cb50ad27ec271f6ceae0a5a752
darajitha <darajitha@users.noreply.github.com>
2017-11-03 19:25:06 +0530

    aa
    aa

--- dbc.js
+++ dbc.js
@@ -1,5 +1,1 @@
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-mongodb.MongoClient.connect("abc", function (err, database) {
-  if (err) {
-    console.log(err, "database error");
-    process.exit(1);
+aaaaa
