79fec9508dbc88f4c581a5bbfe4ccc562860ccb1
Chandima Ranaweera <chan4lk@gmail.com>
2017-11-17 07:51:12 +0530

    last camera selected.

--- public/js/enter.js
+++ public/js/enter.js
@@ -1,19 +1,33 @@
 var ParkApp = ParkApp || {};
 ParkApp.reserve = function () {
-    var scanner = new Instascan.Scanner({
-        video: document.getElementById('preview')
-    });
-    scanner.addListener('scan', function (content) {
-        console.log(content);
-        alert(content);
-    });
-    Instascan.Camera.getCameras().then(function (cameras) {
-        if (cameras.length > 0) {
-            scanner.start(cameras[0]);
-        } else {
-            console.error('No cameras found.');
-        }
-    }).catch(function (e) {
-        console.error(e);
-    });
+    if (Instascan) {
+        var scanner = new Instascan.Scanner({
+            video: document.getElementById('preview')
+        });
+        scanner.addListener('scan', function (content) {
+            console.log(content);
+            alert(content);
+        });
+        Instascan.Camera.getCameras().then(function (cameras) {
+            if (cameras.length > 0) {
+                var cameraSelect = document.getElementById('cameras');
+               
+                scanner.start(cameras[cameras.length -1]);
+            } else {
+                console.error('No cameras found.');
+            }
+        }).catch(function (e) {
+            console.error(e);
+        });
+    }
+}
+
+ParkApp.initCamera = function () {
+    if (Instascan) {
+        Instascan.Camera.getCameras().then(function (camera) {
+
+        }).catch(function (err) {
+            alert(err);
+        });
+    }
 }
